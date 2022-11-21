22a364276cfe50afa246a7fbe6e13eb7a8462ca8
Greg Billings <greg@backcountrydev.com>
2022-11-21 14:20:25 -0500

    Update SmartWings Zigbee Shades.groovy

--- Packages/Smartwings/SmartWings Zigbee Shades.groovy
+++ Packages/Smartwings/SmartWings Zigbee Shades.groovy
@@ -145,8 +145,7 @@ def configure() {
 						zigbee.readAttribute(CLUSTER_POWER, BATTERY_PERCENTAGE_REMAINING)
 
 	def cmds = zigbee.configureReporting(CLUSTER_WINDOWCOVERING, WINDOWCOVERING_ATTR_LIFTPERCENTAGE, 0x20, 1, 3600, 0x00) +
-			   //zigbee.configureReporting(CLUSTER_POWER, POWER_ATTR_BATTERY, 0x20, 1, 3600, 0x01) +
-               zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, BATTERY_PERCENTAGE_REMAINING, DataType.UINT8, 30, 21600, 0x01)
+                   zigbee.configureReporting(zigbee.POWER_CONFIGURATION_CLUSTER, BATTERY_PERCENTAGE_REMAINING, DataType.UINT8, 30, 21600, 0x01)
 
 	if (debugEnable) log.info "configure() --- cmds: $cmds"
     
